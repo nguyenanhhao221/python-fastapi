1d8a92bf346425e775009e7f52e4bd7ad21b7f14
Hao Nguyen <hao@haonguyen.tech>
2023-06-17 17:30:08 +0700

    run basic unicorn server and first few route for fast api

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-def main(arg):
-    pass
+from typing import Union
+
+from fastapi import FastAPI
+from uvicorn import Config, Server
+
+# Initialize fast api
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn = Server(
+        Config(
+            "main:app",
+            port=8080,
+        ),
+    )
+    uvicorn.run()
